eb1a9415d696528c30e06affaf7ffff7b447ea71
AniketKumarC007 <fabaniket246@gmail.com>
2024-02-02 19:05:29 +0530

    FINAL COMMIT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
-
+import myimg from "./giphy.gif" ;
 function App() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = () => {
+    setLoading(true);
+
+    // Fetch data from JSONPlaceholder API
+    const randomStartIndex = Math.floor(Math.random() * 190) + 1;
+    axios.get(`https://jsonplaceholder.typicode.com/todos?_start=${randomStartIndex}&_limit=7`)
+      .then(response => {
+        setItems(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>List of Items Fetched Using Open API JSON Placeholder</h3>
+      {loading ? (
+        <p><img src={myimg}></img></p>
+      ) : (
+        <div>
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                <span>{item.title}</span>
+              </li>
+            ))}
+          </ul>
+          <button onClick={fetchData}>Refresh</button>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
